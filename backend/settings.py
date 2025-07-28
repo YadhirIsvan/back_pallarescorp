bf7452f62312c16afaca22b8776eb1ba9c3cd425
YadhirIsvan <yadhirisvan11@gmail.com>
2025-07-28 17:04:05 -0600

    Oculto claves sensibles usando dotenv

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+
 MERCADOPAGO_ACCESS_TOKEN = os.getenv("MERCADOPAGO_ACCESS_TOKEN")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j^urftckh)br#r79)a+24z8w4t9bv_hj)$y7!#_+h#+)5vv9be'
+SECRET_KEY = os.getenv('SECRET_KEY', 'clave-insegura-solo-para-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
