a74aa4e8725c64a604b7c518097788d72b5d6fe3
YadhirIsvan <yadhirisvan11@gmail.com>
2025-07-29 12:42:19 -0600

    añadiendo cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,10 @@ ALLOWED_HOSTS = [
 ]
 
 
+CORS_ALLOWED_ORIGINS = [
+    "https://www.pallarescorp.com",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -51,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'payments',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
